cf5f6e8963927ffbbf7312faae98e59e3ee89053
Jeremy Hairr <jeremyhairr@gmail.com>
2026-02-26 07:57:43 -0500

    test

--- app.py
+++ app.py
@@ -2,18 +2,8 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-@app.route("/", methods=["GET", "POST"])
+@app.route("/")
 def home():
-    if request.method == "POST":
-        name = request.form["name"]
-        age = request.form["age"]
-
-        question = "In John 6:44, Jesus says, 'No one comes to the Father unless He first draws them.' Does this mean that salvation is of the Lord?"
-        answer = "Salvation is of the Lord. Paul says in Ephesians 2 that salvation is of the Lord so that no one can boast."
-
-        return render_template("result.html", name=name, age=age, question=question, answer=answer)
-
-    return render_template("index.html")
-
+    return "Hello from Render!"
 if __name__ == "__main__":
     app.run(debug=True)
