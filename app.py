16a9b8888e54de5f6bcc85deb6d501f9080a3235
Jeremy Hairr <jeremyhairr@gmail.com>
2026-02-26 08:10:36 -0500

    test

--- app.py
+++ app.py
@@ -2,8 +2,25 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/", methods=["GET", "POST"])
 def home():
-    return "Hello from Render!"
+    if request.method == "POST":
+        name = request.form["name"]
+        age = request.form["age"]
+
+        question = "In John 6:44..."
+        answer = "Salvation is of the Lord..."
+
+        return render_template(
+            "result.html",
+            name=name,
+            age=age,
+            question=question,
+            answer=answer
+        )
+
+    return render_template("index.html")
+
+
 if __name__ == "__main__":
     app.run(debug=True)
